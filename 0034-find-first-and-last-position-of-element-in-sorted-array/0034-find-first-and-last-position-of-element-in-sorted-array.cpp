221183769654520ffc29f1e25c955657d254f52f
Aakash gupta <aakashsahuu0188@gmail.com>
2026-09-17 11:44:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.62%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+
+    int findrightmost(vector<int>& nums, int target, int n) {
+        int l = 0;
+        int r = n - 1;
+        int right_most = -1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                right_most = mid;
+                l = mid + 1;       // search right
+            }
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return right_most;
+    }
+
+
+    int findleftmost(vector<int>& nums, int target, int n) {
+        int l = 0;
+        int r = n - 1;
+        int left_most = -1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) {
+                left_most = mid;
+                r = mid - 1;       // search left
+            }
+            else if (nums[mid] < target) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return left_most;
+    }
+
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        int left_most = findleftmost(nums, target, n);
+        int right_most = findrightmost(nums, target, n);
+
+        return {left_most, right_most};
+    }
+};
